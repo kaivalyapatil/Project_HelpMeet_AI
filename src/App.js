3d6893c97d9b4fe4055632a84a6e2578e49add5e
Kaivalya Patil <93880188+kaivalyapatil@users.noreply.github.com>
2022-04-15 01:40:27 +0530

    commit message

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { Navbar } from './Navbar';
+import { Faq } from './Faq';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { scroller } from 'react-scroll';
+
+export const App = () => {
+  const alanBtnInstance = useRef(null);
+  const [index, setIndex] = useState(null);
+  const [currentFaqId, setCurrentFaqId] = useState(null);
+  const [toggleColorFlag, setToggleColorFlag] = useState(false);
+
+  useEffect(() => {
+    if (!alanBtnInstance.current) {
+      alanBtnInstance.current = alanBtn({
+        key: '7225a84a45b2e39c32250ad85a24883f2e956eca572e1d8b807a3e2338fdd0dc/stage',
+        onCommand: commandData => {
+          if (commandData.command === 'gotoFaq') {
+            scroller.scrollTo(`accordion-button-${commandData.faqId}`, {
+              duration: 800,
+              delay: 0,
+              smooth: 'easeInOutQuart',
+            });
+            setIndex(commandData.faqId - 1);
+            setCurrentFaqId(commandData.faqId);
+          } else if (commandData.command === 'toggleColorMode') {
+            setToggleColorFlag(flag => !flag);
+          }
+        },
+      });
+    }
+  }, []);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar toggleColorFlag={toggleColorFlag} />
+      <Faq
+        index={index}
+        setIndex={setIndex}
+        currentFaqId={currentFaqId}
+        setCurrentFaqId={setCurrentFaqId}
+      />
+    </ChakraProvider>
+  );
+};
